e30ae0cf73b210a44adcb5bf7bf8564b88d7253e
m.naumann <m.naumann@windot.de>
2023-02-25 18:26:15 +0100

    add basix sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import {createSignal} from "solid-js";
-import {invoke} from "@tauri-apps/api/tauri";
-import styles from "App.scss"
-import "./App.css";
+import { JSX } from 'solid-js';
+import './App.scss';
 
-function App() {
-    const [greetMsg, setGreetMsg] = createSignal("");
-    const [name, setName] = createSignal("");
-
-    async function greet() {
-        // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-        setGreetMsg(await invoke("greet", {name: name()}));
-    }
-
-    return (
-        <main class={styles.}>
-
-        </main>
-    );
+export default function App(): JSX.Element {
+  return <main>
+    <section></section>
+    <section></section>
+  </main>;
 }
-
-export default App;
