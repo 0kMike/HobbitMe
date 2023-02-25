a7e0a8f66594bd88183256f2374a0689beafe047
m.naumann <m.naumann@windot.de>
2023-02-25 23:45:34 +0100

    add custom tauri menu

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,8 @@
     windows_subsystem = "windows"
 )]
 
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -10,7 +12,22 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    let quit_menu_item = CustomMenuItem::new("quit".to_string(), "Quit");
+    let file_submenu = Submenu::new("File", Menu::new().add_item(quit_menu_item));
+    let font_hobbit = CustomMenuItem::new("hobbit".to_string(), "Hobbit");
+    let font_classic = CustomMenuItem::new("classic".to_string(), "Classic");
+    let font_submenu = Submenu::new(
+        "Font",
+        Menu::new().add_item(font_hobbit).add_item(font_classic),
+    );
+    let settings_submenu = Submenu::new("Setting", Menu::new().add_submenu(font_submenu));
+    let about_menu_item = CustomMenuItem::new("about".to_string(), "About...");
+    let menu = Menu::new()
+        .add_submenu(file_submenu)
+        .add_submenu(settings_submenu)
+        .add_item(about_menu_item);
     tauri::Builder::default()
+        .menu(menu)
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
